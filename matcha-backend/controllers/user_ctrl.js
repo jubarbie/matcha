401188bca3250bcb8b8ad6df59eed54c08445789
Jules Barbier <jubarbie@student.42.fr>
2018-03-01 14:23:53 +0100

    stuff

--- matcha-backend/controllers/user_ctrl.js
+++ matcha-backend/controllers/user_ctrl.js
@@ -8,136 +8,152 @@ var ImageModel = require('../models/image_model');
 var ctrl = {};
 
 ctrl.getFullUser = (logged, login, callback) => {
-	UsersModel.getFullDataUserWithLogin(logged.login, login, (err, rows, fields) => {
-		if (!err && rows.length > 0) {
-			var user = formatUser(rows[0], logged);
-			callback(user);
-		} else {
-			console.log(err);
-			callback(null);
-		}
-	});
+    UsersModel.getFullDataUserWithLogin(logged.login, login, (err, rows, fields) => {
+        if (!err && rows.length > 0) {
+            var user = formatUser(rows[0], logged);
+            callback(user);
+        } else {
+            console.log(err);
+            callback(null);
+        }
+    });
 };
 
 function getDistance(pos_from, pos_to) {
 
-	let R = 6371; // Radius of the earth in km
-  let dLat = deg2rad(pos_to.lat - pos_from.lat);  // deg2rad below
-  let dLon = deg2rad(pos_to.lon - pos_from.lon);
-  let a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(pos_from.lat)) * Math.cos(deg2rad(pos_to.lat)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  let d = R * c; // Distance in km
+    let R = 6371; // Radius of the earth in km
+    let dLat = deg2rad(pos_to.lat - pos_from.lat); // deg2rad below
+    let dLon = deg2rad(pos_to.lon - pos_from.lon);
+    let a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(pos_from.lat)) * Math.cos(deg2rad(pos_to.lat)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c; // Distance in km
 
-	return d;
+    return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+    return deg * (Math.PI / 180)
 }
 
-ctrl.getConnectedUser = function (login, callback) {
-	UsersModel.getUserWithLogin(login, function(err, rows, fields) {
-		if (!err && rows.length > 0) {
-			var user = rows[0];
-			TalkModel.getUserTalks(login, function(err, talks, fields) {
-				talks.map(function (talk) {
-					talk.messages = [];
-					talk.new_message = "";
-					return talk;
-				});
-				user.talks = talks;
-				user.photos = [];
-				user.interested_in = (user.interested_in) ? user.interested_in.split(',') : [];
-				user.tags = (user.tags) ? user.tags.split(',') : [];
-				ImageModel.getImagesFromUserId(user.id, function(err, imgs, fields){
-					if (!err && imgs.length > 0) {
-						user.photos = imgs.map(function (img) {
-							return [img.id, config.root_url + config.upload_path + img.src];
-						});
-					}
-					if (user.localisation) {
-						user.localisation = JSON.parse(user.localisation);
-					}
-					callback(user);
-				});
-			});
-		} else {
-			callback(null);
-		}
-	});
+ctrl.getConnectedUser = function(login, callback) {
+    UsersModel.getUserWithLogin(login, function(err, rows, fields) {
+        if (!err && rows.length > 0) {
+            var user = rows[0];
+            TalkModel.getUserTalks(login, function(err, talks, fields) {
+                talks.map(function(talk) {
+                    talk.messages = [];
+                    talk.new_message = "";
+                    return talk;
+                });
+                user.talks = talks;
+                user.photos = [];
+                user.interested_in = (user.interested_in) ? user.interested_in.split(',') : [];
+                user.tags = (user.tags) ? user.tags.split(',') : [];
+                ImageModel.getImagesFromUserId(user.id, function(err, imgs, fields) {
+                    if (!err && imgs.length > 0) {
+                        user.photos = imgs.map(function(img) {
+                            return [img.id, config.root_url + config.upload_path + img.src];
+                        });
+                    }
+                    if (user.localisation) {
+                        user.localisation = JSON.parse(user.localisation);
+                    }
+                    callback(user);
+                });
+            });
+        } else {
+            callback(null);
+        }
+    });
 };
 
 ctrl.getRelevantUsers = (logged, callback) => {
 
-  var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
-  var int_in = (logged.gender) ? logged.gender : "M";
+    var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
+    var int_in = (logged.gender) ? logged.gender : "M";
 
-  UsersModel.getRelevantProfiles(logged.login, gender, int_in, function(err, rows, fields) {
-    if (!err) {
-      var users = rows.map(function (u) {
-        return formatUser(u, logged);
-      });
-      callback(users);
-		} else {
-			callback(null);
-		}
-	});
+    UsersModel.getRelevantProfiles(logged.login, gender, int_in, function(err, rows, fields) {
+        if (!err) {
+            var users = rows.map(function(u) {
+                return formatUser(u, logged);
+            });
+            callback(users);
+        } else {
+            callback(null);
+        }
+    });
 };
 
 function formatUser(row, logged) {
-	row.has_talk = false;
-	row.photos = (row.photos) ? row.photos.split(",") : [];
-	row.photos = row.photos.map(function (img) {
-		return config.root_url + config.upload_path + img;
-	});
-	row.tags = (row.tags) ? row.tags.split(',') : [];
-	row.visitor = (row.visitor != null) ? true : false;
-	if (row.localisation && logged.localisation) {
-		row.distance = getDistance(JSON.parse(logged.localisation), JSON.parse(row.localisation))
-	}
-	row.localisation = "secret information";
-	row.liked = (row.liked > 0);
-	row.liking = (row.liking > 0);
+    row.has_talk = false;
+    row.photos = (row.photos) ? row.photos.split(",") : [];
+    row.photos = row.photos.map(function(img) {
+        return config.root_url + config.upload_path + img;
+    });
+    row.tags = (row.tags) ? row.tags.split(',') : [];
+    row.visitor = (row.visitor != null) ? true : false;
+    if (row.localisation && logged.localisation) {
+        row.distance = getDistance(JSON.parse(logged.localisation), JSON.parse(row.localisation))
+    }
+    row.localisation = "secret information";
+    row.liked = (row.liked > 0);
+    row.liking = (row.liking > 0);
 
-	return row;
+    return row;
 }
 
 ctrl.getVisitors = (logged, callback) => {
 
-  var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
-  var int_in = (logged.gender) ? logged.gender : "M";
+    var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
+    var int_in = (logged.gender) ? logged.gender : "M";
 
-  UsersModel.getVisitors(logged.login, gender, int_in, function(err, rows, fields) {
-    if (!err) {
-      var users = rows.map(function (u) {
-        return formatUser(u, logged);
-      });
-      callback(users);
-		} else {
-			callback(null);
-		}
-	});
+    UsersModel.getVisitors(logged.login, gender, int_in, function(err, rows, fields) {
+        if (!err) {
+            var users = rows.map(function(u) {
+                return formatUser(u, logged);
+            });
+            callback(users);
+        } else {
+            callback(null);
+        }
+    });
+};
+
+ctrl.getMatchers = (logged, callback) => {
+
+    var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
+    var int_in = (logged.gender) ? logged.gender : "M";
+
+    UsersModel.getVisitors(logged.login, gender, int_in, function(err, rows, fields) {
+        if (!err) {
+            var users = rows.map(function(u) {
+                return formatUser(u, logged);
+            });
+            callback(users);
+        } else {
+            callback(null);
+        }
+    });
 };
 
 ctrl.getLikers = (logged, callback) => {
 
-  var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
-  var int_in = (logged.gender) ? logged.gender : "M";
+    var gender = (logged.interested_in) ? logged.interested_in.split(',') : [];
+    var int_in = (logged.gender) ? logged.gender : "M";
 
-  UsersModel.getLikers(logged.login, gender, int_in, function(err, rows, fields) {
-    if (!err) {
-      var users = rows.map(function (u) {
-        return formatUser(u, logged);
-      });
-      callback(users);
-		} else {
-			callback(null);
-		}
-	});
+    UsersModel.getLikers(logged.login, gender, int_in, function(err, rows, fields) {
+        if (!err) {
+            var users = rows.map(function(u) {
+                return formatUser(u, logged);
+            });
+            callback(users);
+        } else {
+            callback(null);
+        }
+    });
 };
 
 module.exports = ctrl;
--- matcha-client/src/User/UserAccountView.elm
+++ matcha-client/src/User/UserAccountView.elm
@@ -1,82 +1,94 @@
-module User.UserAccountView exposing (view, viewEditAccount, viewChangePwd)
+module User.UserAccountView exposing (view, viewChangePwd, viewEditAccount)
 
+import Commands exposing (..)
+import FormUtils exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Keyed exposing (..)
 import Json.Decode as JD
 import Models exposing (..)
-import Commands exposing (..)
 import Msgs exposing (..)
-import FormUtils exposing (..)
 import User.UserModel exposing (..)
 import Utils exposing (..)
 
 
-view : Model ->  Html Msg
+view : Model -> Html Msg
 view model =
     case model.session of
-        Just s -> Html.Keyed.node "div" [] [("div", viewAccount model s.user)]
-        _ -> text "no session..."
+        Just s ->
+            Html.Keyed.node "div" [] [ ( "div", viewAccount model s.user ) ]
+
+        _ ->
+            text "no session..."
+
 
 viewAccount : Model -> SessionUser -> Html Msg
 viewAccount model user =
-  div [class "content"]
-    [ div [ class "row" ]
-      [ div [class "twelve columns"] <|
-        [ div [] <|
-          (if user.status ==  Incomplete then
-            [ div [] [ text "Please complete your profile" ] ]
-          else
-            []) ++
-          (if user.status ==  ResetPassword then
-            [ div [] [ text "Don't forget to change your password" ] ]
-          else
-            [])
+    div [ class "content" ]
+        [ div [ class "row" ]
+            [ div [ class "twelve columns" ] <|
+                [ div [] <|
+                    (if user.status == Incomplete then
+                        [ div [] [ text "Please complete your profile" ] ]
+                     else
+                        []
+                    )
+                        ++ (if user.status == ResetPassword then
+                                [ div [] [ text "Don't forget to change your password" ] ]
+                            else
+                                []
+                           )
+                ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "six columns" ]
+                [ h2 [] [ text "Infos" ]
+                , div [] [ text user.username ]
+                , div [] [ text user.fname, text " ", text user.lname ]
+                , div [] [ text user.email ]
+                , div [] [ text user.bio ]
+                , a [ href "/#/edit_account" ] [ text "Edit infos" ]
+                , br [] []
+                , a [ href "/#/edit_password" ] [ text "Change password" ]
+                ]
+            , div [ class "six columns" ]
+                [ h2 [] [ text "Interest" ]
+                , viewTagSection model user
+                ]
+            ]
+        , viewImages model user
+        , div [ class "row" ]
+            [ hr [] []
+            , h2 [] [ text "Localisation" ]
+            , div [ id "map" ] []
+            , button [ onClick Localize ] [ text "Localize me" ]
+            ]
         ]
-      ]
-    , div [ class "row"]
-      [ div [class "six columns"]
-          [ h2 [] [text "Infos"]
-          , div [] [text user.username]
-          , div [] [text user.fname, text " ",text user.lname]
-          , div [] [text user.email]
-          , div [] [text user.bio]
-          , a [ href "/#/edit_account" ] [ text "Edit infos" ]
-          , br [] []
-          , a [ href "/#/edit_password" ] [ text "Change password" ]
-          ]
-      , div [ class "six columns"]
-          [ h2 [][text "Interest"]
-          , viewTagSection model user
-          ]
-      ]
-    , viewImages model user
-    , div [class "row"]
-          [ hr [] []
-          , h2 [] [text "Localisation"]
-          , div [ id "map" ] []
-          , button [ onClick Localize ][ text "Localize me" ]
-          ]
-      ]
+
 
 viewImages : Model -> SessionUser -> Html Msg
 viewImages model user =
-  div [ class "row gallery" ]
-      [  hr [][]
-      , h2 [] [ text "Photos" ]
-      , (if List.length user.photos > 0 then
-         div [] <| List.map (\(id_, s) ->
-           div [ style [("background", "url(" ++ s ++ ") center center no-repeat")], class "img-box" ]
-                [ button [ class "del", onClick <| DeleteImg id_ ] [ icon "fas fa-times" ]
-                ]
-          ) user.photos
-         else div [] [ text "You haven't uploaded any pictures yet"] )
-    , (if (List.length user.photos) < 5 then
-       viewNewImgeForm model
-      else
-        div [][])
-    ]
+    div [ class "row gallery" ]
+        [ hr [] []
+        , h2 [] [ text "Photos" ]
+        , if List.length user.photos > 0 then
+            div [] <|
+                List.map
+                    (\( id_, s ) ->
+                        div [ style [ ( "background", "url(" ++ s ++ ") center center no-repeat" ) ], class "img-box" ]
+                            [ button [ class "del", onClick <| DeleteImg id_ ] [ icon "fas fa-times" ]
+                            ]
+                    )
+                    user.photos
+          else
+            div [] [ text "You haven't uploaded any pictures yet" ]
+        , if List.length user.photos < 5 then
+            viewNewImgeForm model
+          else
+            div [] []
+        ]
+
 
 viewNewImgeForm : Model -> Html Msg
 viewNewImgeForm model =
@@ -89,9 +101,9 @@ viewNewImgeForm model =
                 Nothing ->
                     text ""
     in
-        div [ class "imageWrapper" ]
-            [ label [ for model.idImg, class "label-upload button" ]
-              [ input
+    div [ class "imageWrapper" ]
+        [ label [ for model.idImg, class "label-upload button" ]
+            [ input
                 [ type_ "file"
                 , accept "image/*"
                 , id model.idImg
@@ -99,9 +111,9 @@ viewNewImgeForm model =
                     (JD.succeed ImageSelected)
                 ]
                 []
-                , text "Upload new image"
-              ]
+            , text "Upload new image"
             ]
+        ]
 
 
 viewImagePreview : Image -> Html Msg
@@ -112,142 +124,202 @@ viewImagePreview image =
         ]
         []
 
+
 viewTagSection : Model -> SessionUser -> Html Msg
 viewTagSection model user =
-  div [] <| List.map (\t ->
-      div [ class "tag dismissable" ]
-          [ text t
-          , div [ class "del pointer", onClick <| RemoveTag t ][ icon "fas fa-times" ]
-          ]) (List.sort user.tags)
+    div [] <|
+        List.map
+            (\t ->
+                div [ class "tag dismissable" ]
+                    [ text t
+                    , div [ class "del pointer", onClick <| RemoveTag t ] [ icon "fas fa-times" ]
+                    ]
+            )
+            (List.sort user.tags)
+            ++ [ viewTagForm model ]
+
 
 viewTagForm : Model -> Html Msg
 viewTagForm model =
-  div [ class "input" ]
-    [ Html.form []
-           [ input [ type_ "text", onInput SearchTag, value model.tagInput ] []
-           , button
-              [ onWithOptions "click" { preventDefault= True, stopPropagation= False } (JD.succeed AddNewTag), type_ "submit" ] [ text "Add" ]
-           ]
-    , div []
-      [ Html.ul [ class "search-list" ] <|
-        List.map (\i -> li [ onClick <| AddTag i, class "pointer" ] [ text i ]) model.searchTag
-      ]
-    ]
+    div [ class "input" ]
+        [ Html.form []
+            [ input [ type_ "text", onInput SearchTag, value model.tagInput ] []
+            , button
+                [ onWithOptions "click" { preventDefault = True, stopPropagation = False } (JD.succeed AddNewTag), type_ "submit" ]
+                [ text "Add" ]
+            , Html.ul [ class "search-list" ] <|
+                List.map (\i -> li [ onClick <| AddTag i, class "pointer" ] [ text i ]) model.searchTag
+            ]
+        ]
+
 
 viewChangePwd : Model -> Html Msg
 viewChangePwd model =
     case model.session of
-      Nothing -> text "no session..."
-      Just s -> viewEditPwdForm model.changePwdForm
+        Nothing ->
+            text "no session..."
 
+        Just s ->
+            viewEditPwdForm model.changePwdForm
 
 
 viewEditAccount : Model -> Html Msg
 viewEditAccount model =
     case model.session of
-      Nothing -> text "no session..."
-      Just s ->
-        div [class "content"]
-          [ viewEditAccountForm model.editAccountForm s.user
-          ]
+        Nothing ->
+            text "no session..."
+
+        Just s ->
+            div [ class "content" ]
+                [ viewEditAccountForm model.editAccountForm s.user
+                ]
+
 
 viewGenderForm : Maybe Gender -> Html Msg
 viewGenderForm gender =
-  div []
-    [ div [] [ label [] [text "I am"] ]
-    , div [ class "row" ]
-          [ label [ for "gmale", class <| "gender-btn six columns" ++ (if (gender == Just M) then " active" else "")]
-                  [ text "Male ", i [class "fas fa-mars"] []
-                  , input
-                      [ name "gender"
-                      , type_ "radio"
-                      , id "gmale"
-                      , onClick <| UpdateGender M
-                      , checked (gender == Just M)
-                      ] []
-                  ]
-            , label [ for "gfemale", class <| "gender-btn six columns" ++ (if (gender == Just F) then " active" else "") ]
-                    [ text "Female ", i [class "fas fa-venus"] []
-                    , input
-                        [ name "gender"
-                        , type_ "radio"
-                        , id "gfemale"
-                        , onClick <| UpdateGender F
-                        , checked (gender == Just F)
-                        ] []
+    div []
+        [ div [] [ label [] [ text "I am" ] ]
+        , div [ class "row" ]
+            [ label
+                [ for "gmale"
+                , class <|
+                    "gender-btn six columns"
+                        ++ (if gender == Just M then
+                                " active"
+                            else
+                                ""
+                           )
+                ]
+                [ text "Male "
+                , i [ class "fas fa-mars" ] []
+                , input
+                    [ name "gender"
+                    , type_ "radio"
+                    , id "gmale"
+                    , onClick <| UpdateGender M
+                    , checked (gender == Just M)
                     ]
+                    []
+                ]
+            , label
+                [ for "gfemale"
+                , class <|
+                    "gender-btn six columns"
+                        ++ (if gender == Just F then
+                                " active"
+                            else
+                                ""
+                           )
+                ]
+                [ text "Female "
+                , i [ class "fas fa-venus" ] []
+                , input
+                    [ name "gender"
+                    , type_ "radio"
+                    , id "gfemale"
+                    , onClick <| UpdateGender F
+                    , checked (gender == Just F)
+                    ]
+                    []
+                ]
             ]
         ]
+
 
 viewIntInForm : List Gender -> Html Msg
 viewIntInForm intIn =
     div []
-      [ div [] [ label [] [text "I am interested in"] ]
-      , div [ class "row"]
-            [ label [ for "imale", class <| "gender-btn six columns" ++ (if (List.member M intIn) then " active" else "") ]
-                    [ text "Males ", i [class "fas fa-mars"] []
-                    , input
-                        [ name "intIn"
-                        , type_ "checkbox"
-                        , id "imale"
-                        , onClick <| UpdateIntIn M
-                        , checked <| List.member M intIn
-                        ] []
+        [ div [] [ label [] [ text "I am interested in" ] ]
+        , div [ class "row" ]
+            [ label
+                [ for "imale"
+                , class <|
+                    "gender-btn six columns"
+                        ++ (if List.member M intIn then
+                                " active"
+                            else
+                                ""
+                           )
+                ]
+                [ text "Males "
+                , i [ class "fas fa-mars" ] []
+                , input
+                    [ name "intIn"
+                    , type_ "checkbox"
+                    , id "imale"
+                    , onClick <| UpdateIntIn M
+                    , checked <| List.member M intIn
                     ]
-              , label [ for "ifemale", class <| "gender-btn six columns" ++ (if (List.member F intIn) then " active" else "") ]
-                      [ text "Females ", i [class "fas fa-venus"] []
-                      , input
-                          [ name "intIn"
-                          , type_ "checkbox"
-                          , id "ifemale"
-                          , onClick <| UpdateIntIn F
-                          , checked <| List.member F intIn
-                          ] []
-                      ]
-              ]
+                    []
+                ]
+            , label
+                [ for "ifemale"
+                , class <|
+                    "gender-btn six columns"
+                        ++ (if List.member F intIn then
+                                " active"
+                            else
+                                ""
+                           )
+                ]
+                [ text "Females "
+                , i [ class "fas fa-venus" ] []
+                , input
+                    [ name "intIn"
+                    , type_ "checkbox"
+                    , id "ifemale"
+                    , onClick <| UpdateIntIn F
+                    , checked <| List.member F intIn
+                    ]
+                    []
+                ]
+            ]
         ]
+
 
 viewEditAccountForm : Form -> SessionUser -> Html Msg
 viewEditAccountForm accountForm user =
-  div [ class "edit-form" ]
-    [ h1 [] [ text <| "Edit account" ]
-    , Html.form [ ] <| List.map (\i -> viewInput (UpdateEditAccountForm i.id) i) accountForm
-      ++
-        [ viewGenderForm user.gender
-        , viewIntInForm user.intIn
-        , input
-              [ onWithOptions
-                  "click"
-                  { preventDefault = True
-                  , stopPropagation = False
-                  }
-                  (JD.succeed SaveAccountUpdates)
-              , class "important-font"
-              , type_ "submit"
-              , value "SAVE"
-              ]
-              []
-         ]
-    , a [ href "/#/account" ][ text "Cancel" ]
-    ]
+    div [ class "edit-form" ]
+        [ h1 [] [ text <| "Edit account" ]
+        , Html.form [] <|
+            List.map (\i -> viewInput (UpdateEditAccountForm i.id) i) accountForm
+                ++ [ viewGenderForm user.gender
+                   , viewIntInForm user.intIn
+                   , input
+                        [ onWithOptions
+                            "click"
+                            { preventDefault = True
+                            , stopPropagation = False
+                            }
+                            (JD.succeed SaveAccountUpdates)
+                        , class "important-font"
+                        , type_ "submit"
+                        , value "SAVE"
+                        ]
+                        []
+                   ]
+        , a [ href "/#/account" ] [ text "Cancel" ]
+        ]
+
 
 viewEditPwdForm : Form -> Html Msg
 viewEditPwdForm formm =
-  div [ class "edit-form" ]
-    [ h1 [] [ text <| "Edit password" ]
-    , Html.form [] <| List.map (\i -> viewInput (UpdateEditPwdForm i.id) i) formm
-      ++ [ input
-              [ onWithOptions
-                  "click"
-                  { preventDefault = True
-                  , stopPropagation = False
-                  }
-                  (JD.succeed ChangePwd)
-              , class "important-font"
-              , type_ "submit"
-              , value "CHANGE PASSWORD"
-              ]
-              []
-         ]
-    , a [ href "/#/account" ][ text "Cancel" ]
-    ]
+    div [ class "edit-form" ]
+        [ h1 [] [ text <| "Edit password" ]
+        , Html.form [] <|
+            List.map (\i -> viewInput (UpdateEditPwdForm i.id) i) formm
+                ++ [ input
+                        [ onWithOptions
+                            "click"
+                            { preventDefault = True
+                            , stopPropagation = False
+                            }
+                            (JD.succeed ChangePwd)
+                        , class "important-font"
+                        , type_ "submit"
+                        , value "CHANGE PASSWORD"
+                        ]
+                        []
+                   ]
+        , a [ href "/#/account" ] [ text "Cancel" ]
+        ]
