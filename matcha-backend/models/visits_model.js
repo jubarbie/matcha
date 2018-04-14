ead34c75e79dc5b2c42ae8838e73fa6014939fbd
Jules BARBIER <jubarbie@e3r12p11.42.fr>
2018-04-14 15:46:49 +0200

    visitnotif

--- matcha-backend/models/visits_model.js
+++ matcha-backend/models/visits_model.js
@@ -3,14 +3,19 @@ const config = require('../config');
 
 let connection = mysql.createConnection(config.database);
 
-exports.addVisit = (from, to, date, cb) =>
-    connection.query('SELECT * FROM visits WHERE user_from = ? AND user_to = ? ', [from, to], (err, rows, fields) => {
-        if (!err && rows.length == 0) {
-            connection.query('INSERT INTO visits (user_from, user_to, date, last) VALUES (?,?,?,0) ', [from, to, date], cb);
-        } else {
-            cb();
-        }
-    });
+exports.addVisit = (from, to, date, cb) => {
+    if (from != to) {
+      connection.query('SELECT * FROM visits WHERE user_from = ? AND user_to = ? ', [from, to], (err, rows, fields) => {
+          if (!err && rows.length == 0) {
+              connection.query('INSERT INTO visits (user_from, user_to, date, last) VALUES (?,?,?,0) ', [from, to, date], cb);
+          } else {
+              cb();
+          }
+      });
+    } else {
+      cb();
+    }
+}
 
 exports.updateVisitLast = (to, date) =>
     connection.query('UPDATE visits SET last = ? WHERE user_to = ?', [date, to]);
